92d5b75cfc175dad34fb2f4f0c2988b7d2160b23
Ezequiel Tejada <et.github@outlook.com>
2020-06-16 11:27:04 +0200

    Add login component logic

--- src/app/auth/login/containers/login/login.component.ts
+++ src/app/auth/login/containers/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/auth/shared/services/auth/auth.service";
+import { Router } from "@angular/router";
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-login",
@@ -6,11 +9,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
-  onSubmit(formValue) {
-    console.log("Login: ", formValue);
+  async onSubmit(form: FormGroup) {
+    const { email, password } = form.value;
+    await this.authService.loginUser({ email, password });
+    this.router.navigate(["/"]);
   }
 }

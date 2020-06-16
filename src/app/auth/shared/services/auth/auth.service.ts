cab1f2220fdd728aa86297f1a8f4286c46785da6
Ezequiel Tejada <et.github@outlook.com>
2020-06-16 11:26:13 +0200

    Add auth logic to authService

--- src/app/auth/shared/services/auth/auth.service.ts
+++ src/app/auth/shared/services/auth/auth.service.ts
@@ -1,10 +1,42 @@
 import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { tap } from "rxjs/operators";
+import { Store } from "src/store";
+
+export interface User {
+  email: string;
+  uid: string;
+  authenticated: boolean;
+}
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  user$ = this.afAuth.authState.pipe(
+    tap((next) => {
+      if (!next) {
+        this.store.set("user", null);
+        return;
+      }
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true,
+      };
+      this.store.set("user", user);
+    })
+  );
 
-  loginUser() {}
-  registerUser() {}
-  logout() {}
+  constructor(private afAuth: AngularFireAuth, private store: Store) {}
+
+  loginUser({ email, password }: { email: string; password: string }) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  registerUser({ email, password }: { email: string; password: string }) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.afAuth.signOut();
+  }
 }

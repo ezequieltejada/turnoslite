d0c34896c49118ed2994e204e113d2735aa7d678
Ezequiel Tejada <et.github@outlook.com>
2020-06-16 02:48:21 +0200

    Added prod and dev env variables.

---
+++ handleFirebaseJSONFiles.js
@@ -0,0 +1,30 @@
+var fs = require("fs-extra");
+var path = require("path");
+
+const validOptions = ["pro", "dev", "production", "development"];
+
+const matchesBetweenEnvsAndValidOptions = process.argv.reduce(
+  (acc, current) => {
+    return validOptions.includes(current.substr(0, 3))
+      ? current.substr(0, 3)
+      : null;
+  }
+);
+
+if (matchesBetweenEnvsAndValidOptions === "pro") {
+  copyProdFirebaseJson();
+} else if (matchesBetweenEnvsAndValidOptions === "dev") {
+  copyDevFirebaseJson();
+} else {
+  console.error("Error: Invalid Argument");
+  return new Error("Invalid Argument");
+}
+
+function copyProdFirebaseJson() {
+  fs.copySync(path.resolve(__dirname, "firebase.prod.json"), "firebase.json");
+}
+function copyDevFirebaseJson() {
+  fs.copySync(path.resolve(__dirname, "firebase.dev.json"), "firebase.json");
+}
+
+//fs.copySync(path.resolve(__dirname,'./init/xxx.json'), 'xxx.json');

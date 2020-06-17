d4b80c2963609cdd62b3475213fa5252a2af3ab2
Ezequiel Tejada <et.github@outlook.com>
2020-06-17 14:11:36 +0200

    Add appointment routing

--- src/appointments/appointments-routing.module.ts
+++ src/appointments/appointments-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AppointmentsComponent } from "./containers/appointments/appointments.component";
-import { AuthGuard } from "../auth/shared/guards/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
     children: [
+      {
+        path: "new",
+        loadChildren:
+          "./create-appointment/create-appointment.module#CreateAppointmentModule",
+      },
+      {
+        path: "update",
+        loadChildren:
+          "./update-appointment/update-appointment.module#UpdateAppointmentModule",
+      },
       {
         path: "",
         component: AppointmentsComponent,

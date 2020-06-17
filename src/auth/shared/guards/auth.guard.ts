777eebf878671294ef7d58cd652939723fb4debc
Ezequiel Tejada <et.github@outlook.com>
2020-06-17 02:40:45 +0200

    Add auth guard to protect routes

---
+++ src/auth/shared/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth/auth.service";
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.user$.pipe(map((user) => !!user));
+  }
+}

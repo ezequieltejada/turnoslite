7eabd2f336091845d45a06306bc67e1a5a203be9
Ezequiel Tejada <et.github@outlook.com>
2020-06-20 14:11:43 +0200

    Fix AppointmentComponent path.

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from "@angular/common";
 import { AppointmentsRoutingModule } from "./appointments-routing.module";
 import { AppointmentsComponent } from "./containers/appointments/appointments.component";
 import { SharedModule } from "./shared/shared.module";
-import { AppointmentComponent } from './components/appointment/appointment.component';
+import { AppointmentComponent } from "./containers/appointment/appointment.component";
 
 @NgModule({
   declarations: [AppointmentsComponent, AppointmentComponent],
   imports: [CommonModule, AppointmentsRoutingModule, SharedModule.forRoot()],
 })
-export class AppointmentsModule {
-}
+export class AppointmentsModule {}
